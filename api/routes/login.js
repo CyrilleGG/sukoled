73ab4ae565accc7e5cbd1cf9fb5b3d1406a12b79
NicolasResmi <nicolas.resmi@etu-webschoolfactory.fr>
2018-06-12 11:46:07 +0200

    Knex testing (connection to DB)

--- api/routes/login.js
+++ api/routes/login.js
@@ -1,14 +1,18 @@
-module.exports = (req, res) => {
-    var users = require('../db/users.js');
-    const _ = require('lodash');
-    const jwt = require('jsonwebtoken');
-    const passportJWT = require('passport-jwt');
-    var jwtOptions = {}
-    const ExtractJwt = passportJWT.ExtractJwt;
-    jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-    jwtOptions.secretOrKey = 'secretKey';
-    
+const db = require('../db');
+var users = require('../db/users.js');
+var pg = require('knex')({
+    client:'pg',
+    connection:'postgres://lpcbrcdmurqmbq:925a55a6db9dc1c88c272a80da7166da31e635b27ff6c1911636095a6a0eb8a2@ec2-46-51-179-166.eu-west-1.compute.amazonaws.com:5432/dbnba4ihfii687',
+});
+const _ = require('lodash');
+const jwt = require('jsonwebtoken');
+const passportJWT = require('passport-jwt');
+var jwtOptions = {}
+const ExtractJwt = passportJWT.ExtractJwt;
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+jwtOptions.secretOrKey = 'secretKey';
 
+module.exports = (req, res) => {
     if (req.body.username && req.body.password) {
         var username = req.body.username;
         var password = req.body.password;

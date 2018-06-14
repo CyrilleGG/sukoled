077b2a12003f9ff3032b3c5297eb4183d268cd9e
Cyrille Guiot-Guillin <cyrille.guiot-guillin@etu-webschoolfactory.fr>
2018-06-14 11:17:21 +0200

    Ajout des filtres

--- front/src/components/Admin/Admin.vue
+++ front/src/components/Admin/Admin.vue
@@ -1,7 +1,25 @@
 <template>
   <div id="Admin" class="container-fluid">
     <Header :role="userInfo.role" />
-    <h1>WELCOME ADMIN !</h1>
+    <div class="row py-5 page-content">
+      <div class="col-lg-8 mx-auto">
+        <div class="row mb-5">
+          <h2 class="col-lg-12 pl-0 text-uppercase">Contributions</h2>
+          <h4 class="col-lg-7 pl-0">Watch the progress of the different contributions</h4>
+          <div class="col-lg-2 ml-auto mr-3">
+            <b-btn id="new-campaign" size="small">New campaign <img src="@/assets/icons/next.png" alt=""></b-btn>
+          </div>
+          <div class="w-100"></div>
+          <div class="col-lg-12 mt-3 pt-1 divider"></div>
+        </div>
+        <div class="row">
+          <FilterContributors />
+          <FilterPeriodicities />
+          <FilterMonths />
+          <FilterSemesters />
+        </div>
+      </div>
+    </div>
     <Footer />
   </div>
 </template>
@@ -9,13 +27,21 @@
 <script>
 import Footer from '@/components/Footer/Footer'
 import Header from '@/components/Header/Header'
+import FilterContributors from '@/components/FilterContributors/FilterContributors'
+import FilterPeriodicities from '@/components/FilterPeriodicities/FilterPeriodicities'
+import FilterMonths from '@/components/FilterMonths/FilterMonths'
+import FilterSemesters from '@/components/FilterSemesters/FilterSemesters'
 
 export default {
   name: 'Admin',
 
   components: {
     Header,
-    Footer
+    Footer,
+    FilterContributors,
+    FilterPeriodicities,
+    FilterMonths,
+    FilterSemesters
   },
 
   data () {
@@ -34,5 +60,32 @@ export default {
 </script>
 
 <style scoped>
+
+#Admin .page-content h2 {
+  font-size: 1.6rem;
+}
+
+#Admin .page-content h4 {
+  font-size: 1.2rem;
+  color: #bbbbbb;
+}
+
+#Admin #new-campaign {
+  background-color: #8e44ad;
+  border-color: #8e44ad;
+}
+
+#Admin #new-campaign:hover {
+  background-color: #793a93;
+  border-color: #793a93;
+}
+
+#Admin #new-campaign img {
+  height: 20px;
+}
+
+#Admin .page-content .divider {
+  background-color: #e7e7e7;
+}
 
 </style>
---
+++ front/src/components/FilterMonths/FilterMonths.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="col-lg-2 pr-0">
+    <b-form-select id="filter-months" v-model="selected" :options="options"></b-form-select>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'FilterMonths',
+  data () {
+    return {
+      selected: 'jan',
+      options: {
+        jan: 'January',
+        feb: 'February',
+        all: 'All'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+
+
+</style>

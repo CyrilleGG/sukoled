cafd51bf68034f1fda0be9d36eacc3d56489a495
Cyrille Guiot-Guillin <cyrille.guiot-guillin@etu-webschoolfactory.fr>
2018-06-26 11:22:33 +0200

    Fix ultime

--- front/src/App.vue
+++ front/src/App.vue
@@ -7,13 +7,16 @@
 <script>
 export default {
   name: 'App',
-
   data () {
     return {
       auth: false
     }
   },
-
+  created () {
+    this.$bus.$on('auth', (status) => {
+      this.$data.auth = status
+    })
+  },
   methods: {
     setAuth (status) {
       this.$data.auth = status
@@ -23,16 +26,13 @@ export default {
 </script>
 
 <style>
-
 body {
   margin: 0;
   font-family: 'Lato', sans-serif;
   color: #2c3e50;
   background-color: #f3f3f3;
 }
-
 a, a:hover, a:link, a:visited {
   color: inherit;
 }
-
 </style>

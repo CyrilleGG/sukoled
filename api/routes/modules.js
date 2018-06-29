e1216e957dcf3542360fcdd902d3b2d4950485a0
NicolasResmi <nicolas.resmi@etu-webschoolfactory.fr>
2018-06-29 10:56:15 +0200

    Ajout de la BDD

--- api/routes/modules.js
+++ api/routes/modules.js
@@ -1,6 +1,6 @@
 var knex = require('knex')({
-    client:'pg',
-    connection: 'postgres://lpcbrcdmurqmbq:925a55a6db9dc1c88c272a80da7166da31e635b27ff6c1911636095a6a0eb8a2@ec2-46-51-179-166.eu-west-1.compute.amazonaws.com:5432/dbnba4ihfii687?ssl=true'
+    client:'mysql',
+    connection: 'mysql://DpNxguDvZwPWcm4u:JQ9hUBgXhAcsnknYBUadaxmscd6R4fVn@wsf-sukoled.czjrbeoyz2de.eu-west-3.rds.amazonaws.com:3306/natixis?ssl=true'
 });
 const db = require('../db');
 var users = require('../db/users.js');

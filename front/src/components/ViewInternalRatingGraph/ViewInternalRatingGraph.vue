512b465051d19ef0a65c85c391ffcefb3e0c5527
NicolasResmi <nicolas.resmi@etu-webschoolfactory.fr>
2018-07-17 16:38:04 +0200

    Ajout de view internal rating sans le graph + header à fix

---
+++ front/src/components/ViewInternalRatingGraph/ViewInternalRatingGraph.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <p> hey </p>
+        <canvas id="myChart" width="400" height="400"></canvas>
+    </div>
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+
+export default {
+    name: 'ViewInternalRatingGraph',
+
+    extends: Bar,
+    mounted () {
+        this.renderChart(data, options)
+    },
+
+    data (){
+        data: {
+        labels:['January', 'February'];
+        datasets:[
+            {
+            label: 'GitHub Commits',
+            backgroundColor: '#f87979',
+            data: [40, 20]
+            }
+        ]}
+    }
+}
+</script>
+
+<style scoped>
+template p {
+    background-color:white;
+}
+</style>
---
+++ front/src/components/ViewInternalRatingListItem/ViewInternalRatingListItem.vue
@@ -0,0 +1,75 @@
+<template>
+
+    <tr class="view-list-item">
+        <td> {{ title }} </td>
+        <td>{{ date_3 }}</td>
+        <td>{{ date_2 }}</td>
+        <td>{{ date_1 }}</td>
+        <td v-if="evolution == '+'" class="text-center">
+            <div class="arrow-up">&rarr;</div>
+        </td>
+        <td v-else-if="evolution == '-'" class="text-center">
+            <div class="arrow-down">&rarr;</div>
+        </td>
+        <td v-else class="text-center">
+            &rarr;
+        </td>
+    </tr>
+            
+</template>
+
+<script>
+import moment from 'moment'
+
+export default {
+  name: 'ViewInternalRatingListItem',
+
+  components: {
+    // c
+  },
+
+  props: [
+    'title',
+    'date_3',
+    'date_2',
+    'date_1',
+    'evolution'
+  ],
+
+  data () {
+    return {
+      // c
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.view-list-item {
+  background: none;
+  transition: 0.2s ease;
+}
+
+.view-list-item:hover {
+  background-color: #8e44ad;
+  color:white;
+}
+
+.table td {
+    border-top:none;
+}
+
+.equal {
+    font-weight:bold;
+}
+
+.arrow-up {
+    transform:rotate(-30deg);
+}
+
+.arrow-down {
+    transform:rotate(30deg);
+}
+
+</style>

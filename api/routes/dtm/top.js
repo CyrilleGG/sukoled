9444b6dfd9a6adacf02070b74f6842c2645e165c
loicsaintroch <hack@loic.io>
2018-07-23 11:05:57 +0200

    Add Total DTM Top 20

--- api/routes/dtm/top.js
+++ api/routes/dtm/top.js
@@ -11,7 +11,7 @@ const oracle = require('../../utilities/oracle')
  */
 
 module.exports = async (req, res) => {
-  const companies = await oracle('dtm')
+  const companies_current = await oracle('dtm')
     .select(
       'LB_RAF_TET AS lb_raf_tet',
       'CD_RAF_TETE AS cd_raf_tete',
@@ -46,9 +46,51 @@ module.exports = async (req, res) => {
     .orderBy('SUM_MT_EXPO_GLOBAL', 'DESC')
     .limit(20);
 
+  const companies_reference = await oracle('dtm')
+    .select(
+      'LB_RAF_TET AS lb_raf_tet',
+      'CD_RAF_TETE AS cd_raf_tete',
+      'ANNEE AS year',
+      'MOIS AS month',
+      'CD_NOTE_INT_LIKE_SP_RAF_TET AS cd_note_int_like_sp_raf_tet',
+  )
+    .max('MT_LIMITE_TGR AS max_mt_limite_tgr')
+    .sum('MT_EXPO_GLOBAL AS sum_mt_expo_global')
+    .where({
+      IND_RAFGEN: '0',
+      IND_RAFPOOL: '0',
+      IND_GBP: '0',
+      IND_GCE: '0',
+      IND_RAFMDO: '0',
+      IND_INTGPE: '0',
+      IND_BAL2_RAF_TET: '1-ENTREPRISES',
+      ANNEE: '2017',
+      MOIS: '12',
+      VERS: '1'
+    })
+    .whereNotIn('CD_RAF_TETE', ['0111747', '0287090'])
+    .andWhere(function () {
+      this.where('MT_EXPO_GLOBAL', '>', '0').orWhere('CD_INSTRUMENT', '=', 'CASH FINANCEMENT CT')
+    })
+    .groupBy(
+      'LB_RAF_TET',
+      'CD_RAF_TETE',
+      'ANNEE', 'MOIS',
+      'CD_NOTE_INT_LIKE_SP_RAF_TET'
+    )
+    .orderBy('SUM_MT_EXPO_GLOBAL', 'DESC')
+    .limit(20);
+
+
   return res.status(200).json({
     statusCode: 200,
     message: 'Ok',
-    data: companies, result
+    data: {
+      companies: companies_current,
+      total: {
+        current: companies_current[0].sum_mt_expo_global,
+        reference: companies_reference[0].sum_mt_expo_global
+      }
+    }
   });
 };

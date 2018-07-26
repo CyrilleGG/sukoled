81da85ad8eba597f19517a399746102ec3c259cf
NicolasResmi <nicolas.resmi@etu-webschoolfactory.fr>
2018-07-26 11:21:58 +0200

    Fix de BaselSegment + avancée version stable static de view/raf

--- front/src/components/ViewRaf/ViewRaf.vue
+++ front/src/components/ViewRaf/ViewRaf.vue
@@ -3,8 +3,8 @@
 
     <Header :role="this.$root.$data.userInfo.role" />
     <header-view/>
-
-    <div class="row py-5 page-content">
+<div class="page-content">
+    <div class="row py-5">
         <div class="col-lg-10 mx-auto">
 
             <div class="row mb-5">
@@ -14,93 +14,110 @@
             <div class="w-100"></div>
             <div class="col-lg-12 mt-3 pt-1 divider"></div>
             </div>
+        </div>
+    </div>
             
-            <div class="row">
-                <div class="col-lg-12 pl-0">
-                    
+    <div class="row">
+        <div class="col-lg-12">
+            
 
-                    <table class="table table-striped border margin-table mr-5">
-                        <thead>
-                            <tr>
-                                <th/>
-                                <th scope="col">Risk Type</th>
-                                <th/>
-                                <th
-                                    v-for="(date, index) in dates"
-                                    v-bind:key="index"
-                                    v-bind:date="date.date"
-                                >
-                                    <div scope="col" class="text-center"> {{ moment(date.date) }} </div>
-                                </th>
-                                <th scope="col" class="text-center raf-limit"> Limit </th>
-                                <th scope="col" class="text-center raf-threshold"> Threshold for declaration </th>
-                                <th scope="col" class="text-center"> Remarks </th>
-                                
-                            </tr>
-                        </thead>
-                        <tbody>
-
-
-                            <tr>
-                                <td class="text-left rotate row-name"><div> I.R Risk </div></td>
-                                <td class="text-center"> Stress Test +/- 200 bp </td>
-                                <td class="text-center"> <span class="dot"/> </td>
-                                <td class="text-center"> - </td>
-                                <td class="text-center"> € 466.9m </td>
-                                <td class="text-center"> -€ 525m </td>
-                                <td class="text-center"> -€ 559.4m </td>
-                                <td class="text-center"> -€ 659.4m </td>
-                                <td colspan="2" class="raf-gradient"> Limit </td>
-                                <td class="text-center"> Remarks </td>
-                            </tr>
-
-                            <tr>
-                                <td rowspan="3" class="text-left rotate row-name row-name-second"><div> Solvency </div></td>
-                                <td class="text-center"> Stress Test +/- 200 bp </td>
-                                <td class="text-center"> <span class="dot"/> </td>
-                                <td class="text-center"> - </td>
-                                <td class="text-center"> € 466.9m </td>
-                                <td class="text-center"> -€ 525m </td>
-                                <td class="text-center"> -€ 559.4m </td>
-                                <td class="text-center"> -€ 659.4m </td>
-                                <td colspan="2" class="raf-gradient"> Limit </td>
-                                <td class="text-center"> Remarks </td>
-                            </tr>
-
-                            <tr>
-                                <td class="text-center"> Stress Test +/- 200 bp </td>
-                                <td class="text-center"> <span class="dot"/> </td>
-                                <td class="text-center"> - </td>
-                                <td class="text-center"> € 466.9m </td>
-                                <td class="text-center"> -€ 525m </td>
-                                <td class="text-center"> -€ 559.4m </td>
-                                <td class="text-center"> -€ 659.4m </td>
-                                <td colspan="2" class="raf-gradient"> Limit </td>
-                                <td class="text-center"> Remarks </td>
-                            </tr>
-
-                            <tr>
-                                <td class="text-center"> Stress Test +/- 200 bp </td>
-                                <td class="text-center"> <span class="dot"/> </td>
-                                <td class="text-center"> - </td>
-                                <td class="text-center"> € 466.9m </td>
-                                <td class="text-center"> -€ 525m </td>
-                                <td class="text-center"> -€ 559.4m </td>
-                                <td class="text-center"> -€ 659.4m </td>
-                                <td colspan="2" class="raf-gradient"> Limit </td>
-                                <td class="text-center"> Remarks </td>
-                            </tr>
+            <table class="table table-striped border mr-5">
+                <thead>
+                    <tr>
+                        <th/>
+                        <th scope="col">Risk Type</th>
+                        <th/>
+                        <th
+                            v-for="(date, index) in dates"
+                            v-bind:key="index"
+                            v-bind:date="date.date"
+                        >
+                            <div scope="col" class="text-center"> {{ moment(date.date) }} </div>
+                        </th>
+                        <th scope="col" class="text-center raf-limit"> Limit </th>
+                        <th scope="col" class="text-center raf-threshold"> Threshold for declaration </th>
+                        <th scope="col" class="text-center"> Remarks </th>
+                        
+                    </tr>
+                </thead>
+                <tbody>
 
 
-                        </tbody>
-                    </table>
+                    <tr>
+                        <td class="text-left rotate row-name"><div> I.R Risk </div></td>
+                        <td class="text-center"> Stress Test +/- 200 bp </td>
+                        <td class="text-center"> <span class="dot"/> </td>
+                        <td class="text-center"> - </td>
+                        <td class="text-center"> € 466.9m </td>
+                        <td class="text-center"> -€ 525m </td>
+                        <td class="text-center"> -€ 559.4m </td>
+                        <td class="text-center"> -€ 659.4m </td>
+                        <td colspan="2" class="raf-gradient"> 
+                            <span class="line-orange"/>
+                            <span class="dot position-3"/>
+                            <span class="limit-text"> -€700M </span>
+                            <span class="line-red"/> 
+                            <span class="threshold-text"> 9% </span>
+                        </td>
+                        <td class="text-center remarks"> Remarks longue pour tester la place que ça pourra prendre </td>
+                    </tr>
+
+                    <tr>
+                        <td rowspan="3" class="text-left rotate row-name row-name-second"><div> Solvency </div></td>
+                        <td class="text-center"> Stress Test +/- 200 bp </td>
+                        <td class="text-center"> <span class="dot"/> </td>
+                        <td class="text-center"> - </td>
+                        <td class="text-center"> € 466.9m </td>
+                        <td class="text-center"> -€ 525m </td>
+                        <td class="text-center"> -€ 559.4m </td>
+                        <td class="text-center"> -€ 659.4m </td>
+                        <td colspan="2" class="raf-gradient"> 
+                            <span class="line-orange"/> 
+                            <span class="line-red"/>
+                        </td>
+                        <td class="text-center"> Remarks </td>
+                    </tr>
+
+                    <tr>
+                        <td class="text-center"> Stress Test +/- 200 bp </td>
+                        <td class="text-center"> <span class="dot"/> </td>
+                        <td class="text-center"> - </td>
+                        <td class="text-center"> € 466.9m </td>
+                        <td class="text-center"> -€ 525m </td>
+                        <td class="text-center"> -€ 559.4m </td>
+                        <td class="text-center"> -€ 659.4m </td>
+                        <td colspan="2" class="raf-gradient">
+                            <span class="line-orange"/> 
+                            <span class="line-red"/> 
+                        </td>
+                        <td class="text-center"> Remarks </td>
+                    </tr>
+
+                    <tr>
+                        <td class="text-center"> Stress Test +/- 200 bp </td>
+                        <td class="text-center"> <span class="dot"/> </td>
+                        <td class="text-center"> - </td>
+                        <td class="text-center"> € 466.9m </td>
+                        <td class="text-center"> -€ 525m </td>
+                        <td class="text-center"> -€ 559.4m </td>
+                        <td class="text-center"> -€ 659.4m </td>
+                        <td colspan="2" class="raf-gradient">
+                            <span class="line-orange"/> 
+                            <span class="line-red"/>
+                        </td>
+                        <td class="text-center"> Remarks </td>
+                    </tr>
 
 
-                </div>
-            </div>
+                </tbody>
+            </table>
+
 
         </div>
     </div>
+</div>
+
+
 
     <Footer />
   </div>
@@ -186,6 +203,10 @@ export default {
   margin-bottom:15px;
 }
 
+.py-5{
+    padding-bottom: 0rem !important;
+}
+
 
 
 
@@ -195,7 +216,7 @@ table {
 }
 
 .table td {
-    vertical-align:baseline;
+    vertical-align:middle;
 }
 
 thead {
@@ -208,8 +229,11 @@ thead {
 
 .raf-gradient {
     height:100%;
-    background: red; /* For browsers that do not support gradients */
-    background: linear-gradient(to right, green, yellow, yellow, red);
+    background: #FF8B94; /* For browsers that do not support gradients */
+    background: linear-gradient(to right, #DCEDC1, #fcf3a7, #fcf3a7, #FF8B94);
+    position:relative;
+    overflow-y:hidden;
+    overflow-x:hidden;
 }
 
 .raf-limit {
@@ -254,6 +278,60 @@ thead {
   background-color: rgb(36, 196, 36);
   border-radius: 50%;
   display: inline-block;
+}
+
+.dot.position-3{
+    position:absolute;
+    left:16%;
+    top:41%;
+}
+
+.line-orange {
+    width: 100%;
+    height: 0px;
+    background-color: transparent;
+    border: rgb(255, 166, 0); /*on supprime le style par défaut*/
+    border-style:dashed;
+    -moz-transform: rotate(-90.0deg);  /* FF3.5+ */
+    -o-transform: rotate(-90.0deg);  /* Opera 10.5 */
+    -webkit-transform: rotate(-90.0deg);  /* Saf3.1+, Chrome */
+    position:absolute;
+    overflow-y:hidden;
+    overflow-x:hidden;
+    right:25%;
+}
+
+.line-red {
+    width: 100%;
+    height: 0px;
+    background-color: transparent;
+    border: rgb(255, 0, 0); /*on supprime le style par défaut*/
+    border-style:dashed;
+    -moz-transform: rotate(-90.0deg);  /* FF3.5+ */
+    -o-transform: rotate(-90.0deg);  /* Opera 10.5 */
+    -webkit-transform: rotate(-90.0deg);  /* Saf3.1+, Chrome */
+    position:absolute;
+    overflow-y:hidden;
+    overflow-x:hidden;
+    left:25%;
+}
+
+.limit-text {
+    background-color:transparent;
+    position:absolute;
+    left:30%;
+    top:35%;
+}
+
+.threshold-text {
+    background-color:transparent;
+    position:absolute;
+    left:79%;
+    top:35%;
+}
+
+tr .remarks{
+    max-width:250px;
 }
 
 </style>
